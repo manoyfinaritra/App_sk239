8b8b736a6f14983dac0d0ebeb9896cce309786db
manoyfinaritra <manoyfinaritra@gmail.com>
2026-09-21 12:37:50 +0300

    api2

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
     // Le dossier `sk239` doit être placé dans le serveur PHP local (ex. htdocs).
-    baseURL : "http://ctmmds.infinityfree.me/"
-    
+    baseURL : "https://ctmmds.infinityfree.me/"
 
 })
 
